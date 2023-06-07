9b34de8a2a1ec53a22382ba1f1730a0934dda831
buhuipao <chenhua22@outlook.com>
2023-06-07 15:30:09 +0800

    提交docs目录

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,4 @@
 
 set -ex
 
-hugo -D && cd public && tar -cvf ../public.tar.gz . && cd -
-
-
-HUGO_BRANCH=hugo
-git push origin HEAD:$HUGO_BRANCH
-
-REMOTE_URL=`git remote -v | grep push | head -n 1 | awk '{print $2}'`
-REMOTE_BRANCH=main
-git subtree push  --prefix=public $REMOTE_URL $REMOTE_BRANCH
+hugo -D && mv public docs
