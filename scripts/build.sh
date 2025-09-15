85575f21152e51e171b4dc532784e40b957f4619
buhuipao <chenhua22@outlook.com>
2025-09-15 18:20:06 +0800

    deploy the site to clouflare

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,16 @@
 
 set -ex
 
-rm -rf public && hugo -D && rm -rf docs &&  mv public docs
+# Allow overriding baseURL via environment variable for multi-deploy setups
+# Usage:
+#   HUGO_BASEURL="https://example.com/" bash -ex scripts/build.sh
+
+BASEURL_FLAG=""
+if [ -n "${HUGO_BASEURL:-}" ]; then
+  BASEURL_FLAG="-b ${HUGO_BASEURL}"
+fi
+
+rm -rf public
+hugo -D ${BASEURL_FLAG}
+rm -rf docs
+mv public docs
